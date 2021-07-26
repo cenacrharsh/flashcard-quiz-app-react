be80fa10db861a251b41336cedb9b9c236369ef6
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-26 20:37:11 +0530

    made Flashcard component

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function Flashcard() {
- return (
-  <div>
-   
-  </div>
- )
+export default function Flashcard({ flashcard }) {
+  return <div>{flashcard.question}</div>;
 }
