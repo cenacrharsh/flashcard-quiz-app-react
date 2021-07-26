59235be17c6c82e8c4d3e7f97deb4f17123820f0
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-26 20:35:33 +0530

    set up FlashcardList component

---
+++ src/FlashcardList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/*Components*/
+import Flashcard from "./Flashcard";
+
+export default function FlashcardList({ flashcards }) {
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <Flashcard 
+        key={flashcard.id}
+        flashcard={flashcard} />;
+      })}
+    </div>
+  );
+}
