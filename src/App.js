3b25c4a044a6eea39d222a0050e70c93717d1b24
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-26 20:31:12 +0530

    set up sample questions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
+/* Components */
+import FlashcardList from "./FlashcardList";
+
 function App() {
-  return null;
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  return <FlashcardList flashcards={flashcards} />;
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "what is 2+2?",
+    answer: "4",
+    option: ["2", "3", "4", "5"],
+  },
+  {
+    id: 2,
+    question: "what is 2+3?",
+    answer: "5",
+    option: ["2", "3", "4", "5"],
+  },
+];
 
 export default App;
