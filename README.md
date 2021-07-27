7d3f65531413fbf87064a837f8b093b0654a478d
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-27 11:27:16 +0530

    getting questions from api

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 <h1 align='center'>FlashCard Quiz App</h1>
 <h2 align='center'><a>✨ DEMO</a></h2>
+
+|![ss](./ss.png)
+
+🎯 Lessons Learned:
+
+- API used : OPEN TRIVIA DATABASE
+- USED AXIOS package for API calls.
+- USED `backface-visibility: hidden`, `transform-style: preserve-3d` & `transform: perspective(1000px)` in CSS
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 /* Components */
 import FlashcardList from "./FlashcardList";
@@ -6,8 +6,36 @@ import FlashcardList from "./FlashcardList";
 /* Styles */
 import "./app.css";
 
+/* Axios */
+import axios from "axios";
+
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer;
+          const options = [...questionItem.incorrect_answers, answer];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: deocdeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  /* Converts the html encoded characters to string then returns it back */
+
+  function deocdeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
 
   return <FlashcardList flashcards={flashcards} />;
 }
